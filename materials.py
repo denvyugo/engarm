2c706b73fcc8b678d72227ab3d3e6b622ed7314f
denvyugo <denvyugo@gmail.com>
2019-08-17 16:33:44 +0300

    bridge pattern

--- materials.py
+++ materials.py
@@ -1,39 +1,64 @@
 import abc
 
+"""
+Classes of materials items: hardware & software
+Hardware orders by pieces or in packages.
+Software orders by licenses.
+"""
+
+
+"""Abstract Factory pattern for create items"""
 
 class Itemfactory(metaclass=abc.ABCMeta):
-    """Abstract Factory class for create items: hardware & software"""
-
+    """Abstract Factory class"""
     @abc.abstractmethod
-    def create_item(self):
+    def create_item(self, **kwargs):
         pass
 
 
 class Hardfactory(Itemfactory):
-
+    """Factory that creates a hardware that ordered by pieces"""
     def create_item(self, name, quantity, cost):
-        return Hardware(name=name, quantity=quantity, cost=cost)
+        order_form = OrderPieces()
+        return Hardware(name=name, quantity=quantity, cost=cost, order_form=order_form)
+
+
+class Packfactory(Itemfactory):
+    """Factory that creates a hardware that ordered by packages"""
+    def create_item(self, name, quantity, cost, **kwargs):
+        order_form = OrderPackages()
+        if 'pack_val' in kwargs:
+            order_form.packvalue(kwargs['pack_val'])
+        return Hardware(name=name, quantity=quantity, cost=cost, order_form=order_form)
 
 
 class Softfactory(Itemfactory):
-
+    """Factory that creates a software that ordered by licenses"""
     def create_item(self, name, quantity, cost):
-        return Software(name=name, quantity=quantity, cost=cost)
+        order_form = OrderLicenses()
+        return Software(name=name, quantity=quantity, cost=cost, order_form=order_form)
 
 
 class Item(metaclass=abc.ABCMeta):
-    """Abstract class for describe item interface"""
+    """
+    Abstract class for describe item interface.
+    Item has an order_form object which implement Bridge pattern for order in right units (pcs, pkg, lns)
+    """
 
     DEFAULT_VAT = 0.2
 
-    def __init__(self, name, quantity, cost):
+    def __init__(self, name, quantity, cost, order_form):
         self._name = name
         self._quantity = quantity
         self._cost = cost
         self._vat = __class__.DEFAULT_VAT
+        self._order_form = order_form
 
     def price(self):
         return self._cost * self._quantity * (1 + self._vat)
+
+    def get_order(self):
+        return self._order_form.get_order(self._name, self._quantity)
 
     @abc.abstractmethod
     def get_info(self):
@@ -41,8 +66,8 @@ class Item(metaclass=abc.ABCMeta):
 
 
 class Hardware(Item):
-    def __init__(self, name, quantity, cost):
-        super().__init__(name, quantity, cost)
+    def __init__(self, name, quantity, cost, order_form):
+        super().__init__(name, quantity, cost, order_form)
         self._vat = 0.2
 
     def get_info(self):
@@ -53,11 +78,47 @@ class Hardware(Item):
 
 
 class Software(Item):
-    def __init__(self, name, quantity, cost):
-        super().__init__(name, quantity, cost)
+    def __init__(self, name, quantity, cost, order_form):
+        super().__init__(name, quantity, cost, order_form)
         self._vat = 0.0
 
     def get_info(self):
         return {'name': self._name, 'quantity': self._quantity, 'price': self.price(), 'vat': 'no vat'}
 
+
+"""Bridge pattern"""
+
+class OrderForm(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def get_order(self, **kwargs):
+        pass
+
+
+class OrderPieces(OrderForm):
+
+    def get_order(self, name, quantity):
+        return f'name: {name} of {quantity} pcs'
+
+
+class OrderLicenses(OrderForm):
+
+    def get_order(self, name, quantity):
+        return f'name: {name} of {quantity} lns'
+
+
+class OrderPackages(OrderForm):
+    DEFAULT_PACK = 1
+
+    def __init__(self,):
+        self._pcs_in_pack = __class__.DEFAULT_PACK
+
+    def packvalue(self, pcs_in_pack):
+        self._pcs_in_pack = pcs_in_pack
+
+    def get_order(self, name, quantity):
+        number_packs = quantity // self._pcs_in_pack
+        if quantity % self._pcs_in_pack:
+            number_packs += 1
+        return f'name: {name} of {number_packs} pkg'
 
--- tests/test_materials.py
+++ tests/test_materials.py
@@ -1,25 +1,33 @@
 import materials
 import unittest
 
+order_pcs = materials.OrderPieces()
+order_lcs = materials.OrderLicenses()
+hard = materials.Hardware(name='hard', quantity=2, cost=10, order_form=order_pcs)
+soft = materials.Software(name='soft', quantity=3, cost=5, order_form=order_lcs)
 
-class Test(unittest.TestCase):
+class TestItems(unittest.TestCase):
 
     def test_hard(self):
-        hard = materials.Hardware(name='hard', quantity=2, cost=10)
-        self.assertEqual(hard.price(), 24)
         self.assertEqual(hard.get_info(), {'name': 'hard',
                                            'quantity': 2,
                                            'price': 24,
                                            'vat': 4})
 
     def test_soft(self):
-        soft = materials.Software(name='soft', quantity=3, cost=5)
         self.assertEqual(soft.price(), 15)
         self.assertEqual(soft.get_info(), {'name': 'soft',
                                            'quantity': 3,
                                            'price': 15,
                                            'vat': 'no vat'})
+    def test_hard_order(self):
+        self.assertEqual(hard.get_order(), 'name: hard of 2 pcs')
 
+    def test_soft_order(self):
+        self.assertEqual(soft.get_order(), 'name: soft of 3 lns')
+
+
+class TestFactory(unittest.TestCase):
     def test_hardfactory(self):
         factory = materials.Hardfactory()
         self.assertEqual(isinstance(factory, materials.Itemfactory), True)
@@ -31,10 +39,12 @@ class Test(unittest.TestCase):
     def test_factories(self):
         factory_h = materials.Hardfactory()
         factory_s = materials.Softfactory()
+        factory_p = materials.Packfactory()
 
         factories = []
         factories.append(factory_h)
         factories.append(factory_s)
+        factories.append(factory_p)
         for factory in factories:
             self.assertEqual(isinstance(factory.create_item(name='item', quantity=2, cost=3), materials.Item), True)
 
@@ -53,3 +63,10 @@ class Test(unittest.TestCase):
         self.assertEqual(isinstance(factory_s.create_item(name='soft',
                                                       quantity=1,
                                                       cost=3), materials.Hardware), False)
+
+        self.assertEqual(isinstance(factory_p.create_item(name='pack',
+                                                          quantity=15,
+                                                          cost=3), materials.Hardware), True)
+
+        pack = factory_p.create_item(name='pack', quantity=15, cost=3, pack_val=10)
+        self.assertEqual(pack.get_order(), 'name: pack of 2 pkg')
